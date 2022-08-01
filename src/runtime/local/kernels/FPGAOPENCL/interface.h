3c4385b5a457554f927d445d56b73e6d784ece2d
pratuszniak <piotr.ratuszniak@intel.com>
2022-08-01 13:35:58 +0000

    FPGA SGEMM preparation for final integration

--- src/runtime/local/kernels/FPGAOPENCL/interface.h
+++
@@ -1,8 +0,0 @@
-#ifndef SGEMM_INTERFACE
-#define SGEMM_INTERFACE
-
-extern int sgemm(const float *A, const float *B, float *C, const int OUTERMOST_I, const int OUTERMOST_J, const int OUTERMOST_K);
-
-#endif
-
-
